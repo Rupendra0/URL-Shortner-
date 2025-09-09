929c31c83ccba66fb5b89cdc704a0bde0d7860f7
Rupendra Gangwar <rupendragangwar07@gmail.com>
2025-09-10 00:07:38 +0530

    Update staticRouter.js

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   try {
+    res.status(200).json({ "message" : "Hey from Backend" , "Server-Health" : "Excellent" });
     res.render("home");
   } catch (err) {
     console.error("Error rendering home page:", err);
